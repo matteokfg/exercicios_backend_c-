d2da4a2ab35bea6b7893d5cad3a15dae9367a0a4
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2023-03-08 09:40:59 -0300

    Add files via upload

---
+++ exercicio_02_list.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exercicio_02_list
+{
+    public static void Main()
+    {
+        int n_convidado = 9;
+        List<string> convidados = new List<string>();
+
+        for (int i = 0; i < n_convidado; i++)
+        {
+            Console.WriteLine($"Nome do {i + 1} convidado:");
+            convidados.Add(Console.ReadLine());
+        }
+
+        Console.WriteLine(convidados.Count);
+        Console.WriteLine(convidados[0]);
+        Console.WriteLine(convidados[1]);
+        Console.WriteLine(convidados[convidados.Count-1]);
+    }
+}
+
