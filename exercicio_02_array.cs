2d1f592d304b649426554fdf8d829f77f0ed277f
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2023-03-08 09:30:26 -0300

    Add files via upload

---
+++ exercicio_02_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+public class Eexercicio_array_02
+{
+    public static void Main()
+    {
+        int n_convidados = 9;
+        // colocaria 900, mas como é um exemplo e nao um caso real, coloquei 9 para nao ocupar muita memoria e ser mais rapido
+        string[] convidados = new string[n_convidados];
+
+        for (int i = 0; i < convidados.Length; i++)
+        {
+            Console.WriteLine($"Escreva o nome do {i+1} convidado:");
+            convidados[i] = Console.ReadLine();
+        }
+        Console.WriteLine("A lista de convidados e:");
+        foreach (var conv in convidados)
+        {
+            Console.WriteLine(conv);
+        }
+    }
+}
