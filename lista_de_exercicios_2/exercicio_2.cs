e747c3689b867787abcf914e7e0b363face981f1
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2023-03-24 20:05:51 -0300

    Create exercicio_2.cs

---
+++ lista_de_exercicios_2/exercicio_2.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEstruturaCondicional
+{ 
+    public class Program 
+    { 
+        public static void Main(string[] args) 
+        { 
+            double a = 0; 
+            double b = 0; 
+            double c = 0; 
+            string entrada = ""; 
+
+            do 
+            { 
+                Console.Write("Primeiro lado: (utilizar virgula para casa decimal)"); 
+                a = double.Parse(Console.ReadLine()); 
+
+                Console.Write("Segundo lado: (utilizar virgula para casa decimal)"); 
+                b = double.Parse(Console.ReadLine()); 
+
+                Console.Write("Terceiro lado: (utilizar virgula para casa decimal)"); 
+                c = double.Parse(Console.ReadLine()); 
+
+                if ((a < (b + c)) && (b < (a + c)) && (c < (a + b)) && (a > 0) && (b > 0) && (c > 0)) 
+                { 
+                    if ((a == b) && (a == c)) 
+                    { 
+                        Console.WriteLine("Equilatero"); 
+                    } 
+                    else 
+                    { 
+                        if ((a == b) || (a == c) || (b == c)) 
+                        { 
+                            Console.WriteLine("Isosceles"); 
+                        } 
+                        else 
+                        { 
+                            Console.WriteLine("Escaleno"); 
+                        } 
+                    } 
+                } 
+                else 
+                { 
+                    Console.WriteLine("Nao eh triangulo"); 
+                } 
+  
+                Console.WriteLine(" Continuar -> 1 \n Sair -> 2"); 
+                entrada = Console.ReadLine(); 
+
+            } while (entrada != "2"); 
+        } 
+    } 
+} 
