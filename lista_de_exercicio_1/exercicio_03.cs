bfc5366cf23cc8f9820626d90cc8473fc39f8093
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2023-03-24 20:25:45 -0300

    Rename exercicio_03.cs to lista_de_exercicio_1/exercicio_03.cs

---
+++ lista_de_exercicio_1/exercicio_03.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exercicio_03
+{
+    public static void Main()
+    {
+        string entrada;
+
+        List<string> frutas = new List<string>();
+        List<string> frutasSemRepetidas = new List<string>();
+
+        Console.WriteLine("Entre com a lista de frutas separadas por ',':");
+        entrada = Console.ReadLine();
+
+        frutas = entrada.Split(",").ToList();
+
+        foreach (var el in frutas)
+        {
+            string frut = el.ToLower();
+
+            string primeiraLetra = frut.Substring(0, 1).ToUpper();
+
+            frut = primeiraLetra + frut.Substring(1, frut.Length-1);
+
+            if (frutasSemRepetidas.Count == 0)
+            {
+                frutasSemRepetidas.Add(frut);
+            }
+            // list.Contains(el) tbm funciona
+            else
+            {
+                if (frutasSemRepetidas.IndexOf(frut) == -1)
+                {
+                    frutasSemRepetidas.Add(frut);
+                }
+            }
+        }
+
+        Console.WriteLine("Resultado: "+Environment.NewLine);
+        foreach (var fruta in frutasSemRepetidas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
