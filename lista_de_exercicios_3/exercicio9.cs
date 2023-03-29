a55077caa636e49aa8d52db77c1aaa26fb329279
Vinicius Honorato Marques <108351403+ViniMarks@users.noreply.github.com>
2023-03-29 11:38:46 -0300

    Create exercicio9.cs

---
+++ lista_de_exercicios_3/exercicio9.cs
@@ -0,0 +1,16 @@
+namespace ListaEstruturaRepetição
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
